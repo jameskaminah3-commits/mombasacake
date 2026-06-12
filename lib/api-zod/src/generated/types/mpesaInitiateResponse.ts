a53f278a1cb0a5e3b0e472ed863bcb78a1c819b5
myboundless <jameskaminah3@gmail.com>
2026-06-12 20:18:55 +0300

    Update mpesaInitiateResponse.ts

--- lib/api-zod/src/generated/types/mpesaInitiateResponse.ts
+++ lib/api-zod/src/generated/types/mpesaInitiateResponse.ts
@@ -10,5 +10,5 @@ export interface MpesaInitiateResponse {
   checkoutRequestId: string;
   responseDescription: string;
   merchantRequestId?: string | null;
-   businessShortCode?: string | null;
+  businessShortCode?: string | null;
 }
