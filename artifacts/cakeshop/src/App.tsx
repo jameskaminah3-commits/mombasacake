5c953d6e28f4115b413299564367cb63e5c1200f
myboundless <jameskaminah3@gmail.com>
2026-06-24 10:03:24 +0300

    Update App.tsx

--- artifacts/cakeshop/src/App.tsx
+++ artifacts/cakeshop/src/App.tsx
@@ -20,6 +20,7 @@ import Checkout from "@/pages/checkout";
 import OrderSuccess from "@/pages/order";
 import Blog from "@/pages/blog";
 import BlogPost from "@/pages/blog-post";
+import CategoryPage from "@/pages/category";
 import Login from "@/pages/login";
 
 // Admin Pages
@@ -84,6 +85,7 @@ function Router() {
           <Switch>
             <Route path="/" component={Home} />
             <Route path="/menu" component={Menu} />
+            <Route path="/category/:slug" component={CategoryPage} />
             <Route path="/cake/:id" component={CakeDetail} />
             <Route path="/cart" component={Cart} />
             <Route path="/checkout" component={Checkout} />
